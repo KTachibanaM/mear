8e2376e6307dd47b5ffd55354da53894a74d9d82
KTachibanaM <whj19931115@gmail.com>
2023-06-15 04:41:30 +0000

    exec mear agent in container

--- scripts/mear-agent-testing.sh
+++ scripts/mear-agent-testing.sh
@@ -6,3 +6,5 @@ docker run -d --name=mear-agent-testing --platform=amd64 debian:bullseye sleep i
 docker network connect mear-network mear-agent-testing
 docker cp ./bin/mear-agent mear-agent-testing:/root
 docker cp ./scripts/demo.json mear-agent-testing:/root
+docker exec -it mear-agent-testing /root/mear-agent /root/demo.json
+docker rm -f mear-agent-testing
