9dea7261b842ca97eb724a5bf6727da6cd7f4d7a
KTachibanaM <whj19931115@gmail.com>
2023-06-19 07:39:40 +0000

    ffmpeg split log by \r so that progress is shown

--- internal/convert_video.go
+++ internal/convert_video.go
@@ -2,10 +2,31 @@ package internal
 
 import (
 	"bufio"
+	"bytes"
 	"os/exec"
 
 	log "github.com/sirupsen/logrus"
 )
+
+func ffmpegSplitLogLines(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	if i := bytes.IndexByte(data, '\r'); i >= 0 {
+		// Found a carriage return character, return the line up to that point
+		return i + 1, data[0:i], nil
+	}
+	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// Found a newline character, return the line up to that point
+		return i + 1, data[0:i], nil
+	}
+	if atEOF {
+		// No more carriage return characters, return the remaining data as a line
+		return len(data), data, nil
+	}
+	// Need more data
+	return 0, nil, nil
+}
 
 func ConvertVideo(ffmpeg_executable, input_video, output_video string, extra_args []string) error {
 	args := []string{"-i", input_video}
@@ -21,7 +42,7 @@ func ConvertVideo(ffmpeg_executable, input_video, output_video string, extra_arg
 	})
 
 	scanner := bufio.NewScanner(stderr)
-	scanner.Split(bufio.ScanLines)
+	scanner.Split(ffmpegSplitLogLines)
 	for scanner.Scan() {
 		m := scanner.Text()
 		context_log.Println(m)
