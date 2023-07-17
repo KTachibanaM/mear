7c9c9fcec5fc2454d54a52394312ffda20bf2d07
KTachibanaM <whj19931115@gmail.com>
2023-07-17 04:07:44 +0000

    revert back s3 session empty credentials

--- internal/s3/create_s3_session.go
+++ internal/s3/create_s3_session.go
@@ -7,13 +7,6 @@ import (
 )
 
 func CreateS3Session(s3_session *S3Session) (*session.Session, error) {
-	if s3_session.AccessKeyId == "" || s3_session.SecretAccessKey == "" {
-		return session.NewSession(&aws.Config{
-			Endpoint:         aws.String(s3_session.EndpointUrl),
-			Region:           aws.String(s3_session.Region),
-			S3ForcePathStyle: aws.Bool(s3_session.PathStyleUrl),
-		})
-	}
 	return session.NewSession(&aws.Config{
 		Endpoint: aws.String(s3_session.EndpointUrl),
 		Region:   aws.String(s3_session.Region),
