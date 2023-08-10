2a03a2c96232acebfcc1aa9a07bc07c9fc7ac875
KTachibanaM <whj19931115@gmail.com>
2023-08-10 23:18:13 +0000

    refactor and add tests for shared destination bucket

--- dev/test-host-shared-destination-bucket.sh
+++ dev/test-host-shared-destination-bucket.sh
@@ -10,7 +10,7 @@ for key in "${!expected_outputs[@]}"; do
     AWS_ACCESS_KEY_ID=minioadmin AWS_SECRET_ACCESS_KEY=minioadmin aws s3 rm --endpoint-url=http://minio:9000 "s3://mear-destination/${key}"
 done
 
-cat ./dev/host-success.json | jq -c | ./dist/mear-host_linux_amd64_v1/mear-host
+cat ./dev/host-shared-destination-bucket.json | jq -c | ./dist/mear-host_linux_amd64_v1/mear-host
 
 for key in "${!expected_outputs[@]}"; do
     actual_size=$(AWS_ACCESS_KEY_ID=minioadmin AWS_SECRET_ACCESS_KEY=minioadmin aws s3api head-object --endpoint-url=http://minio:9000 --bucket mear-destination --key "${key}" --query ContentLength)
